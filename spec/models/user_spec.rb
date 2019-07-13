6156b14b28a0dc64146ebf14224b3f3b2fda760b
JerYoMat <jeremy.emata@gmail.com>
2019-07-13 19:32:37 -0400

    added user tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    User.destroy_all
+    LearnHelp.destroy_all
+  end
+
+  let(:user_attributes) do
+    {
+      name: 'some name',
+      email: 'user@test.com',
+      password: 'password',
+      github_username: 'JerYo',
+      status: 'student'
+    }
+  end
+
+  let(:missing_name) { user_attributes.except(:name) }
+  let(:missing_email) { user_attributes.except(:email) }
+  let(:missing_password) { user_attributes.except(:password) }
+  let(:missing_github_username) { user_attributes.except(:github_username) }
+  let(:missing_status) { user_attributes.except(:status) }
+  let(:unit) { Unit.create(title: 'Rails', order: 1) }
+  let(:topic) { Topic.create(title: 'basics', order: 1, unit_id: 1) }
+
+  let(:learn_helper_one) do
+    LearnHelper.new(
+      title: 'test 1',
+      url: 'www.railstutorial.org',
+      topic_id: 1
+    )
+  end
+
+  let(:learn_helper_two) do
+    LearnHelper.new(
+      title: 'test 2',
+      url: 'www.secondrailstutorial.org',
+      topic_id: 1
+    )
+  end
+
+  it 'is valid when expected' do
+    expect(User.new(user_attributes)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    expect(User.new(missing_name)).to be_invalid
+  end
+
+  it 'is invalid without email' do
+    expect(User.new(missing_email)).to be_invalid
+  end
+
+  it 'requires a plausible email format' do
+    expect(false)
+  end
+
+  it 'has secure password' do
+    user = User.create(user_attributes)
+    expect(User.new(missing_password)).to be_invalid
+    expect(user.authenticate('dog')).to eq(false)
+    expect(user.authenticate('password')).to eq(user)
+  end
+
+  it 'is invalid without github username' do
+    expect(User.new(missing_github_username)).to be_invalid
+  end
+
+  it 'is invalid without status' do
+    expect(User.new(missing_status)).to be_invalid
+  end
+
+  it 'defaults to admin => false' do
+    user = User.create(user_attributes)
+    expect(user.admin).to eq(false)
+  end
+
+  it 'has many learn helpers' do
+    user = User.create(user_attributes)
+    expect(user.learn_helpers).to eq([learn_helper_one, learn_helper_two])
+  end
+
+  it 'has many topics, through helpers' do
+    user = User.create(user_attributes)
+    expect(user.topics).to eq([topic])
+  end
+end
