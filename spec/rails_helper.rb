8eb57c0c7f98e3c44f4e884ec8e90dd9bf5f761f
JerYoMat <jeremy.emata@gmail.com>
2019-07-15 11:14:51 -0400

    included RequestSpecHelper and ControllerSpecHelper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,21 +1,15 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
+
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
 require 'rspec/rails'
-
+# Add additional requires below this line. Rails is not loaded until this point!
 require 'database_cleaner'
-
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+ActiveRecord::Migration.maintain_test_schema!
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -25,8 +19,10 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
   config.use_transactional_fixtures = true
 
   config.include FactoryBot::Syntax::Methods
@@ -41,11 +37,13 @@ RSpec.configure do |config|
       example.run
     end
   end
-
+  
   config.infer_spec_type_from_file_location!
 
   config.include RequestSpecHelper
   config.include ControllerSpecHelper
+  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
 end
