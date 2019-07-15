0e39e760f2ea94456a834732e0a0b817d06bae05
JerYoMat <jeremy.emata@gmail.com>
2019-07-15 11:20:08 -0400

    added minimal controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,24 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-
-  # GET /users
-  def index
-    @users = User.all
-    json_response(@users)
-  end
-
-  # POST /users
+  skip_before_action :authorize_request, only: :create
+  # POST /signup
+  # return authenticated token upon signup
   def create
-    @user = User.create!(user_params)
-    json_response(@user, :created)
-  end
-
-  # GET /users/:id
-  def show
-    json_response(@user)
-  end
-
-  # PUT /users/:id
-  def update
-    @user.update(user_params)
-    head :no_content
-  end
-
-  # DELETE /users/:id
-  def destroy
-    @user.destroy
-    head :no_content
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
   end
 
   private
 
   def user_params
-    # whitelist params
-    params.permit(:name, :github_username, :email, :password, :status)
-  end
-
-  def set_user
-    @user = User.find(params[:id])
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation,
+      :github_username,
+      :status
+    )
   end
 end
