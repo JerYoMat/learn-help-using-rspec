72543181d1c50a3a32955bc246fd91eb4763c814
JerYoMat <jeremy.emata@gmail.com>
2019-07-15 11:10:40 -0400

    added auth, signup and topic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :users 
+  resources :users
+  resources :topics, only: [:index, :show]
+  post 'auth/login', to: 'authentication#authenticate'
+  post '/signup', to: 'users#create'
 end
